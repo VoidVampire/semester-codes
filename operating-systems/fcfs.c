53dab60fa41a6e3c19dca79d091e3940670c94b6
VoidVampire <yashdsingh2004@gmail.com>
2023-06-07 19:57:25 +0530

    updated fcfs.c

--- operating-systems/fcfs.c
+++ operating-systems/fcfs.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
-int main(){
+int main() {
     int num;
     printf("How many processes?\n");
     scanf("%d", &num);
     int process[num];
     int bt[num];
-    int wt[num];
-    int tt[num];
-    int avgtt=0, avgwt=0;
-    printf("Enter Process ID and corresponding burst time and arrival time\n");
-    for (int i=0; i < num; i++){
-        scanf("%d",&process[i]);
-        scanf("%d",&bt[i]);
+    float wt[num];
+    float tt[num];
+    float avgtt = 0.0, avgwt = 0.0;
+    printf("Enter Process ID and corresponding burst time\n");
+    for (int i = 0; i < num; i++) {
+        scanf("%d", &process[i]);
+        scanf("%d", &bt[i]);
     }
 
-
     for (int i = 0; i < num; i++) {
-        wt[i] = 0;
+        wt[i] = 0.0;
         for (int j = 0; j < i; j++) {
             wt[i] += bt[j];
         }
@@ -26,15 +25,14 @@ int main(){
         avgtt += tt[i];
     }   
     printf("\nWT:\n");
-    for (int i=0; i < num; i++){
-        printf("P%d: %d\n", process[i], wt[i]);
+    for (int i = 0; i < num; i++) {
+        printf("P%d: %.2f\n", process[i], wt[i]);
     }
-    printf("Avg WT:%d\n", avgwt/num);
+    printf("Avg WT: %.2f\n", avgwt / num);
     printf("TT:\n");
-    for (int i=0; i < num; i++){
-        printf("P%d: %d\n", process[i], tt[i]);
+    for (int i = 0; i < num; i++) {
+        printf("P%d: %.2f\n", process[i], tt[i]);
     }
-    printf("Avg TT:%d\n", avgtt/num);
+    printf("Avg TT: %.2f\n", avgtt / num);
     return 0;
-
 }
