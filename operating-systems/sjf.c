5e559ff37e1fc9807268a5a1cdb3d0a7102ec394
VoidVampire <yashdsingh2004@gmail.com>
2023-06-07 20:23:38 +0530

    added sjf without arrival time

---
+++ operating-systems/sjf.c
@@ -0,0 +1,71 @@
+
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void swapp(float *a, float *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num;
+    printf("How many processes?\n");
+    scanf("%d", &num);
+    int process[num];
+    int bt[num];
+    float wt[num];
+    float tt[num];
+    float avgtt = 0.0, avgwt = 0.0;
+    printf("Enter Process ID and corresponding burst time and arrival time\n");
+    for (int i = 0; i < num; i++) {
+        scanf("%d", &process[i]);
+        scanf("%d", &bt[i]);
+    }
+
+    for (int i = 0; i < num; i++) {
+        for (int j = i + 1; j < num; j++) {
+            if (bt[i] > bt[j]) {
+                swap(&bt[i], &bt[j]);
+                swap(&process[i], &process[j]);
+            }
+        }
+    }
+
+    for (int i = 0; i < num; i++) {
+        wt[i] = 0.0;
+        for (int j = 0; j < i; j++) {
+            wt[i] += bt[j];
+        }
+        tt[i] = wt[i] + bt[i];
+        avgwt += wt[i];
+        avgtt += tt[i];
+    }   
+    
+    for (int i = 0; i < num; i++) {
+        for (int j = i + 1; j < num; j++) {
+            if (process[i] > process[j]) {
+                swap(&process[i], &process[j]);
+                swapp(&wt[i], &wt[j]);
+                swapp(&tt[i], &tt[j]);
+            }
+        }
+    }
+    
+    printf("\nWT:\n");
+    for (int i = 0; i < num; i++) {
+        printf("P%d: %.2f\n", process[i], wt[i]);
+    }
+    printf("Avg WT: %.2f\n", avgwt / num);
+    printf("TT:\n");
+    for (int i = 0; i < num; i++) {
+        printf("P%d: %.2f\n", process[i], tt[i]);
+    }
+    printf("Avg TT: %.2f\n", avgtt / num);
+    return 0;
+}
